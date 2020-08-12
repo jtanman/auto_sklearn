2ddce553e2cc718eb5737d653cbf0b3c55395068
jtanman <jtanman@gmail.com>
2020-08-11 17:08:05 -0700

    fixed custom loss function

--- auto_sk/auto_sk.py
+++ auto_sk/auto_sk.py
@@ -1,3 +1,7 @@
+import os
+import pickle
+import time
+
 import autosklearn.regression
 import feather
 import numpy as np
@@ -5,35 +9,104 @@ import pandas as pd
 import sklearn.datasets
 import sklearn.metrics
 import sklearn.model_selection
-import os
-import time
-import pickle
+from sklearn.metrics._regression import _check_reg_targets
+from sklearn.utils.validation import _num_samples, check_array, check_consistent_length
+
+# def mse_weighted(y, yhat):
+#     # weighted error squaring the error and then doubling it if the actual delivery is late/greater
+#     err = np.mean(np.where(y > yhat, 2 * ((y - yhat) ** 2), (y - yhat) ** 2))
+#     return err
 
 
-def mse_weighted(y, yhat):
-    # weighted error squaring the error and then doubling it if the actual delivery is late/greater
-    err = np.mean(np.where(y > yhat, 2 * ((y - yhat) ** 2), (y - yhat) ** 2))
-    return err
+# def rmse_weighted(y_true, y_pred):
+#     # weighted error squaring the error and then doubling it if the actual delivery is late/greater
+#     # sum = 0
+#     # for i in range(len(y)):
+#     #     y_temp = y[i]
+#     #     yhat_temp = yhat[i]
+#     #     if y_temp > yhat_temp:
+#     #         sum += 2 * ((y_temp - yhat_temp) ** 2)
+#     #     else:
+#     #         sum += (y_temp - yhat_temp) ** 2
+
+#     # return np.sqrt(sum / len(y))
+#     print(y_true)
+#     print(y_pred)
+#     sample_weight = np.ones(len(y_true))
+#     for i in range(len(y_true)):
+#         if y_true[i] > y_pred[i]:
+#             sample_weight[i] = 2
+#     # sample_weight[y_true > y_pred] = 2
+#     # sample_weight = np.where(y_true > y_pred, 2, 1)
+#     output_errors = np.sqrt(np.average((y_true - y_pred) ** 2, axis=0,
+#                                weights=sample_weight))
+#     return output_errors
 
 
-def rmse_weighted(y_true, y_pred):
-    # weighted error squaring the error and then doubling it if the actual delivery is late/greater
-    # sum = 0
-    # for i in range(len(y)):
-    #     y_temp = y[i]
-    #     yhat_temp = yhat[i]
-    #     if y_temp > yhat_temp:
-    #         sum += 2 * ((y_temp - yhat_temp) ** 2)
-    #     else:
-    #         sum += (y_temp - yhat_temp) ** 2
-    
-    # return np.sqrt(sum / len(y))
-    sample_weight = np.ones(len(y_true))
-    sample_weight[y_true > y_pred] = 2
-    # sample_weight = np.where(y_true > y_pred, 2, 1)
-    output_errors = np.sqrt(np.average((y_true - y_pred) ** 2, axis=0,
-                               weights=sample_weight))
-    return output_errors
+def mean_squared_error_weighted(y_true, y_pred, *, sample_weight=None, multioutput='uniform_average', squared=False):
+    """Mean squared error weighted regression loss
+    Read more in the :ref:`User Guide <mean_squared_error>`.
+    Parameters
+    ----------
+    y_true : array-like of shape (n_samples,) or (n_samples, n_outputs)
+        Ground truth (correct) target values.
+    y_pred : array-like of shape (n_samples,) or (n_samples, n_outputs)
+        Estimated target values.
+    sample_weight : array-like of shape (n_samples,), optional
+        Sample weights.
+    multioutput : string in ['raw_values', 'uniform_average'] \
+                or array-like of shape (n_outputs)
+        Defines aggregating of multiple output values.
+        Array-like value defines weights used to average errors.
+        'raw_values' :
+            Returns a full set of errors in case of multioutput input.
+        'uniform_average' :
+            Errors of all outputs are averaged with uniform weight.
+    squared : boolean value, optional (default = True)
+        If True returns MSE value, if False returns RMSE value.
+    Returns
+    -------
+    loss : float or ndarray of floats
+        A non-negative floating point value (the best value is 0.0), or an
+        array of floating point values, one for each individual target.
+    Examples
+    --------
+    >>> from sklearn.metrics import mean_squared_error
+    >>> y_true = [3, -0.5, 2, 7]
+    >>> y_pred = [2.5, 0.0, 2, 8]
+    >>> mean_squared_error(y_true, y_pred)
+    0.375
+    >>> y_true = [3, -0.5, 2, 7]
+    >>> y_pred = [2.5, 0.0, 2, 8]
+    >>> mean_squared_error(y_true, y_pred, squared=False)
+    0.612...
+    >>> y_true = [[0.5, 1],[-1, 1],[7, -6]]
+    >>> y_pred = [[0, 2],[-1, 2],[8, -5]]
+    >>> mean_squared_error(y_true, y_pred)
+    0.708...
+    >>> mean_squared_error(y_true, y_pred, squared=False)
+    0.822...
+    >>> mean_squared_error(y_true, y_pred, multioutput='raw_values')
+    array([0.41666667, 1.        ])
+    >>> mean_squared_error(y_true, y_pred, multioutput=[0.3, 0.7])
+    0.825...
+    """
+    y_type, y_true, y_pred, multioutput = _check_reg_targets(y_true, y_pred, multioutput)
+    check_consistent_length(y_true, y_pred, sample_weight)
+    sample_weight = np.where(y_true > y_pred, 2, 1)
+    output_errors = np.average((y_true - y_pred) ** 2, axis=0, weights=sample_weight)
+
+    if not squared:
+        output_errors = np.sqrt(output_errors)
+
+    if isinstance(multioutput, str):
+        if multioutput == 'raw_values':
+            return output_errors
+        elif multioutput == 'uniform_average':
+            # pass None as weights to np.average: uniform mean
+            multioutput = None
+
+    return np.average(output_errors, weights=multioutput)
 
 
 def run():
@@ -55,7 +128,7 @@ def run():
     print("Use self defined accuracy metric")
     rmse_weighted_scorer = autosklearn.metrics.make_scorer(
         name="rmse_weighted",
-        score_func=rmse_weighted,
+        score_func=mean_squared_error_weighted,
         optimum=0,
         greater_is_better=False,
         needs_proba=False,
@@ -63,8 +136,8 @@ def run():
     )
 
     automl = autosklearn.regression.AutoSklearnRegressor(
-        time_left_for_this_task=10*60,
-        per_run_time_limit=5*60,
+        time_left_for_this_task=10 * 60,
+        per_run_time_limit=5 * 60,
         ml_memory_limit=8192,
         include_preprocessors=['no_preprocessing',],
         tmp_folder=f'{os.getcwd()}/autosklearn_regression_example_tmp',
